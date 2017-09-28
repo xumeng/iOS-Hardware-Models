1e4ee86a5de4e619a2d3a82025a048b4dfdf1504
XuMeng <gosu@foxmail.com>
2017-09-28 15:53:11 +0800

    init commit

---
+++ HardwareModelUtils.h
@@ -0,0 +1,19 @@
+//
+//  HardwareModelUtils.h
+//  CareerPlanning
+//
+//  Created by Amon Xu on 2016/6/11.
+//  Copyright © 2016年 Amon Xu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HardwareModelUtils : NSObject
+
+/**
+ *  get iOS device's version
+ *
+ */
++ (NSString*)deviceVersion;
+
+@end
---
+++ README.md
@@ -0,0 +1,11 @@
+# iOS-Hardware-Models
+Basic code to get the Hardware Model of an iOS device in Objective-C.
+
+Example:
+```
+if ([deviceString isEqualToString:@"iPhone4,1"])    return @"iPhone 4S";
+```
+
+
+References:
+[https://en.wikipedia.org/wiki/List_of_iOS_devices](https://en.wikipedia.org/wiki/List_of_iOS_devices)
